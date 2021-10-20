22303125745abc53617245aaf8195eb1af8cfb8e
EloiGry <eloi.grychta@gmail.com>
2021-10-20 10:39:00 +0200

    modif

---
+++ index.js
@@ -0,0 +1,45 @@
+
+$(function() {
+
+    var characters = []
+
+    $.ajax({
+        url :"https://thronesapi.com/api/v2/Characters",
+        success: function(data) {
+            
+            characters = data
+            displayList(characters)
+           
+        }
+    })
+    
+
+    
+    $("input").keyup(function() {
+        var str = $("#input").val().toLowerCase()
+        var filterArray = characters.filter(function(character){
+            return character.fullName.toLowerCase().includes(str)
+        })     
+        displayList(filterArray)     
+    })
+
+    function displayList (array) {
+        $('#characters').html("")
+        array.forEach(function(character) {
+            $('#characters').append(`
+            <div class="character" id="cards">
+            <img src="${character.imageUrl}">
+            <h3 class="name" id="fullName"> ${character.fullName} </h3>
+            <p class="title"> ${character.title}</p>
+            </div>
+            `)  
+            
+        
+            
+        })
+    }
+})
+
+
+
+
